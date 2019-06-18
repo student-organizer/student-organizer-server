8bb4a23de2be9e37e895b55ec06377a9d2a81fc5
Frederik Reiter <git@frederikreiter.de>
2019-06-18 19:51:06 +0200

    Add boost lib
    Add boost lib in NuGet
    Add sample code for a simple echo server

--- StudentOrganizerServer/StudentOrganizerServer.cpp
+++ StudentOrganizerServer/StudentOrganizerServer.cpp
@@ -2,8 +2,111 @@
 
 #include "pch.h"
 #include <iostream>
+#include <boost/asio.hpp>
 
-int main()
+using boost::asio::ip::tcp;
+
+class session
+	: public std::enable_shared_from_this<session>
 {
-    std::cout << "Hello World!\n"; 
+public:
+	session(tcp::socket socket)
+		: socket_(std::move(socket))
+	{
+	}
+
+	void start()
+	{
+		do_read();
+	}
+
+private:
+	void do_read()
+	{
+		auto self(shared_from_this());
+		socket_.async_read_some(boost::asio::buffer(data_, max_length),
+			[this, self](boost::system::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				do_write(length);
+			}
+		});
+	}
+
+	void do_write(std::size_t length)
+	{
+		auto self(shared_from_this());
+		char outdata[max_length];
+
+		for(int i = length-1; i >= 0; --i)
+		{
+			outdata[length-i-1] = data_[i];
+		}
+
+		async_write(socket_, boost::asio::buffer(outdata, length),
+			[this, self](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec)
+			{
+				do_read();
+			}
+		});
+	}
+
+	tcp::socket socket_;
+	enum { max_length = 1024 };
+	char data_[max_length];
+};
+
+class server
+{
+public:
+	server(boost::asio::io_context& io_context, short port)
+		: acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+	{
+		do_accept();
+	}
+
+private:
+	void do_accept()
+	{
+		acceptor_.async_accept(
+			[this](boost::system::error_code ec, tcp::socket socket)
+		{
+			if (!ec)
+			{
+				std::make_shared<session>(std::move(socket))->start();
+			}
+
+			do_accept();
+		});
+	}
+
+	tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+	// Sample code from: https://www.boost.org/doc/libs/1_70_0/doc/html/boost_asio/example/cpp11/echo/async_tcp_echo_server.cpp
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: async_tcp_echo_server <port>\n";
+			return 1;
+		}
+
+		boost::asio::io_context io_context;
+
+		server s(io_context, std::atoi(argv[1]));
+
+		io_context.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
 }
